7bbd49fd632f5e1f569170032785b6e1dc9b2acd
arencrg <clarence13cariaga@gmail.com>
2017-09-20 16:43:02 +0800

    Day 3 Commit

--- server/server.js
+++ server/server.js
@@ -49,11 +49,10 @@ MongoClient.connect(uri, function(err, db) {
   db.close();
 });
 
-
-//mongoose.connect(serverConfig.mongoURL, (error) => {
-//  if (error) {
-//    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-//    throw error;
+mongoose.connect(serverConfig.mongoURL, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
   }
 
   // feed some dummy data in DB.
